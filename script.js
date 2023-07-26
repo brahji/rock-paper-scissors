3236021f3a7afbbe19ac95c42725bb9bf52a2549
brahji <55807478+brahji@users.noreply.github.com>
2023-07-25 20:35:06 -0400

    created playRound function

--- script.js
+++ script.js
@@ -5,15 +5,31 @@ function getComputerChoice() {
     return choiceList[randomChoice];
 }
 
-console.log(getComputerChoice())
+
 
 function getPlayerChoice() {
     let playerChoice = prompt('Make your choice, rock paper or scissors');
     return playerChoice.toLowerCase();
 }
 
-console.log(getPlayerChoice())
+
+
+const playerchoice = getPlayerChoice();
+const computerchoice = getComputerChoice();
+
+
+console.log(computerchoice)
+console.log(playerchoice)
 
 function playRound(getPlayerChoice, getComputerChoice) {
-    
+    if (playerchoice == computerchoice) {console.log("Tie!");}
+        else if (playerchoice == 'rock' && computerchoice == 'scissors') {console.log("You win! Rock beats scissors.");}
+        else if (playerchoice == 'rock' && computerchoice == 'paper') {console.log("You lose! Paper beats rock.");} 
+        else if (playerchoice == 'scissors' && computerchoice == 'paper') {console.log("You win! Scissors beats paper.");} 
+        else if (playerchoice == 'scissors' && computerchoice == 'rock') {console.log("You lose! Rock beats scissors.");} 
+        else if (playerchoice == 'paper' && computerchoice == 'scissors') {console.log("You lose! Scissors beats paper.");} 
+        else if (playerchoice == 'paper' && computerchoice == 'rock') {console.log("You win! Paper beats rock.");} 
+
 }
+
+playRound()
