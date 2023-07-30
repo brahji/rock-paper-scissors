28d91546b11a281828f5a0449d1f9bc80711d3ea
brahji <55807478+brahji@users.noreply.github.com>
2023-07-30 17:55:11 -0400

    added game function which plays a 5 round version of playRound function

--- script.js
+++ script.js
@@ -13,23 +13,34 @@ function getPlayerChoice() {
 }
 
 
-
+let playerscore = 0;
+let computerscore = 0;
 const playerchoice = getPlayerChoice();
 const computerchoice = getComputerChoice();
+const gamescore = playerscore + " - " + computerscore;
 
 
-console.log(computerchoice)
-console.log(playerchoice)
+
+console.log(computerchoice);
+console.log(playerchoice);
 
 function playRound(getPlayerChoice, getComputerChoice) {
-    if (playerchoice == computerchoice) {console.log("Tie!");}
-        else if (playerchoice == 'rock' && computerchoice == 'scissors') {console.log("You win! Rock beats scissors.");}
-        else if (playerchoice == 'rock' && computerchoice == 'paper') {console.log("You lose! Paper beats rock.");} 
-        else if (playerchoice == 'scissors' && computerchoice == 'paper') {console.log("You win! Scissors beats paper.");} 
-        else if (playerchoice == 'scissors' && computerchoice == 'rock') {console.log("You lose! Rock beats scissors.");} 
-        else if (playerchoice == 'paper' && computerchoice == 'scissors') {console.log("You lose! Scissors beats paper.");} 
-        else if (playerchoice == 'paper' && computerchoice == 'rock') {console.log("You win! Paper beats rock.");} 
+    if (playerchoice == computerchoice) {console.log("Tie!" + gamescore);}
+        else if (playerchoice == 'rock' && computerchoice == 'scissors') {console.log("You win! Rock beats scissors." + gamescore) && playerscore + 1;}
+        else if (playerchoice == 'rock' && computerchoice == 'paper') {console.log("You lose! Paper beats rock." + gamescore) && computerscore + 1;} 
+        else if (playerchoice == 'scissors' && computerchoice == 'paper') {console.log("You win! Scissors beats paper." + gamescore) && playerscore + 1;} 
+        else if (playerchoice == 'scissors' && computerchoice == 'rock') {console.log("You lose! Rock beats scissors." + gamescore) && computerscore + 1;} 
+        else if (playerchoice == 'paper' && computerchoice == 'scissors') {console.log("You lose! Scissors beats paper." + gamescore) && computerscore + 1;} 
+        else if (playerchoice == 'paper' && computerchoice == 'rock') {console.log("You win! Paper beats rock." + gamescore) && playerscore + 1;} 
 
 }
+function getScore()
+function game(){
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
+};
 
-playRound()
+game();
